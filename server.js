4cd5c9718872950c6489162e41b93a6f64cd280d
Icedwolf <romaxino@gmail.com>
2019-08-13 01:22:36 +0000

    criado model add require-dir for requiring models, first Product created

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express(); //instanciando
 
+// Iniciando Db
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+);
+
+//import all models
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// req = request | res = response
 app.get('/', (req, res) => {
-    res.send("Hello World"); //resposta
+    Product.create({
+        title: 'React Native',
+        description: 'build native apps with react',
+        url: 'https://github.com/facebook/react-native'
+    });
+    return res.send("Hello World"); //resposta
 });
 
 app.listen(3001); //porta
