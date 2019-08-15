89779521cfc95e0304c700517f7de5e76c94b227
Icedwolf <romaxino@gmail.com>
2019-08-14 22:08:43 -0300

    CRUD CORS Paginação

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
-const app = express(); //instanciando
+const app = express(); //servidor
+app.use(express.json()); //permitir requests json
+app.use(cors()); //enable cross-origin acess
 
-// Iniciando Db
+// conectando Db
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true
 });
@@ -14,4 +17,4 @@ requireDir("./src/models");
 
 app.use("/api", require("./src/routes"));
 
-app.listen(3001); //porta
+app.listen(3001); //porta do servidor
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,8 +4,43 @@ const Product = mongoose.model("Product");
 
 module.exports = {
   async index(req, res) {
-    const products = await Product.find();
+    /* desestruturação para pegar a query de página atual
+    com um valor default para a variavel @page */
+    const { page = 1 } = req.query;
+    const products = await Product.paginate(
+      {
+        //condições
+      },
+      //página atual | limite
+      { page, limit: 10 } //page shortsyntax for page: page
+    );
 
     return res.json(products);
+  },
+
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+
+    return res.json(product);
+  },
+
+  async store(req, res) {
+    const product = await Product.create(req.body);
+
+    return res.json(product);
+  },
+
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true //retorna o produto atualizado no update, sem o new retorna o produto antigo
+    });
+
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+
+    return res.send();
   }
 };
